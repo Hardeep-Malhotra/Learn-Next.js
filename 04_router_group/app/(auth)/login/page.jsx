f466e0a15a7876ae46c7932e94d93c431c7d0b33
Hardeep-Malhotra <hardeepmalhotra022@gmail.com>
2026-07-01 16:48:48 +0530

    feat: Learn Router Grouping.

---
+++ 04_router_group/app/(auth)/login/page.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <form
+        action=""
+        style={{ justifyContent: "center", backgroundColor: "lightblue" }}
+      >
+        Email:
+        <input type="text" name="" id="" />
+        <br />
+        Password:
+        <input type="password" name="" />
+        <br />
+        <button type="submit">Submit</button>
+        <br />
+      </form>
+    </div>
+  );
+};
+
+export default page;
---
+++ 04_router_group/app/(dashboard)/profile/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1>This is Profile page</h1>
+    </div>
+  );
+};
+
+export default page;

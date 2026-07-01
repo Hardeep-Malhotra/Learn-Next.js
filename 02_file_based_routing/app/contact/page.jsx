7d4ce49c3b8cf63d3733dc8849adf9c4b186a59f
Hardeep-Malhotra <hardeepmalhotra022@gmail.com>
2026-07-01 15:29:56 +0530

    feat: add About and Contact pages with basic layout

---
+++ 02_file_based_routing/app/contact/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-white  bg-amber-400 border-1 rounded-2xl">
+        This is Contact page
+      </h1>
+    </div>
+  );
+};
+
+export default page;
